40c701affc6a5423e4648a04a615aad2e7803353
Carlos Eduardo Rodrigues Diógenes <cerdiogenes@gmail.com>
2013-05-13 21:28:51 -0300

    applicatio create with 'rails new hosted_forums --skip-test-unit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HostedForums::Application.config.secret_token = '17083a597ea9b26dcfdb60b8b24c9ec63d39ede4802e0144694b45c76c59cc5c4815faffeb4688c70b9f138ae53e34fcf84c852e292be78d6ef6e57fd837a887'
